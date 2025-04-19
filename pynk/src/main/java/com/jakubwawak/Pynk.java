a9b21dbbf5a38e1e4b16f63e5e1e31a45616f432
Jakub Wawak <kubawawak@gmail.com>
2025-04-19 13:07:08 +0200

    pynk service build changes

--- pynk/src/main/java/com/jakubwawak/Pynk.java
+++ pynk/src/main/java/com/jakubwawak/Pynk.java
@@ -19,8 +19,8 @@ import com.jakubwawak.maintanance.Properties;
  */
 public class Pynk {
 
-    public static final String VERSION = "1.0.0";
-    public static final String BUILD = "pynk19042025REV03";
+    public static final String VERSION = "1.1.0";
+    public static final String BUILD = "pynk20042025REV01";
     public static final boolean debug = false;
 
     public static DatabaseEngine databaseEngine;
